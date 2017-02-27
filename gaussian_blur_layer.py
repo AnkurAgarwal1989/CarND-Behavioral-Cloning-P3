51d46366ccd5ff05991c2fdba14d29ace7be650e
Ankur Agarwal <ankuraga@gmail.com>
2017-02-26 23:50:44 -0800

    Some corrections to weights init

--- gaussian_blur_layer.py
+++ gaussian_blur_layer.py
@@ -7,21 +7,8 @@ Created on Thu Feb 25 16:03:36 2017
 
 from keras import backend as K
 from keras.engine.topology import Layer
-import numpy as np
-
-#Function takes a vector and generates a 1D Gaussian Signal, with mu and sigma
-def gauss_1D(x, mu, sigma):
-    return np.exp(-(x - float(mu))**2 / (2*float(sigma)**2))
-
-#Function to generate 2D Gaussian Kernel given a mean, sigma
-def gauss_kernel(kernel_size, mu, sigma):
-    kernel_size = max(3, kernel_size)
-    x = np.linspace(-kernel_size, kernel_size, kernel_size)
-    x_g = gauss_1D(x, mu, sigma)
-    
-    kernel = np.outer(np.transpose(x_g), x_g)
-    kernel = kernel / np.sum(kernel)
-    return kernel
+import functools
+from keras import initializations
 
 ##Layer to implement Gaussian Blurring on 3 channel RGB images
 #The layer does not have trainable weights. Weights are fixed to a Gaussian Kernel
@@ -29,25 +16,37 @@ def gauss_kernel(kernel_size, mu, sigma):
 #Arguments: kernel_size: size of gaussian kernel to be created
 #           weights: weights for the Gaussian filter
 class GaussBlurLayer(Layer):
-    def __init__(self, kernel_size, weights, **kwargs):
-		self.initial_weights = weights
+    def __init__(self, kernel_size, weights, init='glorot_uniform', dim_ordering='default', **kwargs):
+        self.kernel_size = kernel_size
+        self.initial_weights = weights
+        self.init = initializations.get(init)
+        if dim_ordering == 'default':
+            dim_ordering = K.image_dim_ordering()
+        self.dim_ordering=dim_ordering
+        #self.input_shape = input_shape
+        #self.batch_input_shape = batch_input_shape
         super(GaussBlurLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        # Create a trainable weight variable for this layer.
-        self.W = self.add_weight(shape=(kernel_size, kernel_size),
-                                 initializer=self.initial_weights,
+        #self.W = self.initial_weights
+        self.W = self.add_weight((self.kernel_size, self.kernel_size, 1, 1),
+                                 initializer=functools.partial(self.init,
+                                 dim_ordering='tf'),
                                  trainable=False)
-        super(GaussBlurLayer, self).build()  # Be sure to call this somewhere!
+        print(len(self.get_weights()))
+        print(self.initial_weights.shape)
+        self.set_weights([self.initial_weights])
+        
+        super(GaussBlurLayer, self).build(input_shape)  # Be sure to call this somewhere!
 
     def call(self, x, mask=None):
-		##Split data along 3rd axis to do 2D convolution for gaussian blurring
-		img = K.split(x, 3, axis=3)
-		
-		for img_c in img:
-			img_c = K.conv2d(img_c, self.W, border_mode='same')
-		x = K.concat(img, axis=3)
-		return x
+        
+        ##Split data along 3rd axis to do 2D convolution for gaussian blurring
+#        img = K.split(x, 3, axis=3)
+#        for img_c in img:
+#            img_c = K.conv2d(img_c, self.W, border_mode='same')
+#        x = K.concat(img, axis=3)
+        return x
 
     def get_output_shape_for(self, input_shape):
         return input_shape
--- model_utils.py
+++ model_utils.py
@@ -10,12 +10,14 @@ Utility functions to initiate training, saving and loading models and weights
 '''
 
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, TensorBoard
-from keras.models import load_model
+from keras.models import load_model, model_from_json, Model, Sequential
+from keras.optimizers import Adam
+import data_generator as gen
 import json
-'''
-TODO
-import generator
-'''
+import os
+
+import numpy as np
+
 #using
 #ModelCheckpoint to save model every few epochs
 #EarlyStopping to finish training if loss doesnt reduce
@@ -23,53 +25,67 @@ import generator
 #TensorBoard to generate TensorFlow friendly logs
 
 #if resuming traning, begin_at_epoch will change, else remains 0
-def begin_training(model, begin_at_epoch=0):
-	model.compile(loss='mean_squared_error', optimizer=Adam(lr=1e-3))
+def begin_training(model, training_samples, validation_samples, begin_at_epoch=0):
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=1e-3))
+    model_checkpoint = ModelCheckpoint(filepath='model.weights.{epoch:02d}-{val_loss:.5f}.h5', verbose=1, save_best_only=True, save_weights_only=True, period=20)
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1)
+    reduce_LR_plateau = ReduceLROnPlateau(factor=0.1, patience=2, verbose=1, epsilon=1e-5)
+    tensorboard_log = TensorBoard(log_dir='./tb_logs', histogram_freq=0, write_graph=True, write_images=False)
 
-	model_checkpoint = ModelCheckpoint(filepath='model.weights.{epoch:02d}-{val_loss:.5f}.h5', verbose=1, save_best_only=True, save_weights_only=True, period=20)
-	early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1)
-	reduce_LR_plateau = ReduceLROnPlateau(factor=0.1, patience=2, verbose=1, epsilon=1e-5)
-	tensorboard_log = TensorBoard(log_dir='./tb_logs', histogram_freq=0, write_graph=True, write_images=False)
-
-	batch_size= 128
-	samples_per_epoch = 8000
-	nb_val_samples = 2000
-	n_epoch = 100
-	
-	fit = model.fit_generator(generator(training_samples, batch_size),
-							  samples_per_epoch=samples_per_epoch,
-							  nb_epoch=n_epoch, verbose=2,
-							  callbacks=[model_checkpoint, early_stopping, reduce_LR_plateau, tensorboard_log],
-							  validation_data=generator(validation_samples, batch_size),
-							  nb_val_samples = nb_val_samples,
-							  initial_epoch = begin_at_epoch)
-	print("Done Training")
+    batch_size= 128
+    samples_per_epoch = 8000
+    nb_val_samples = 2000
+    n_epoch = 100
+    
+    fit = model.fit_generator(gen.generator(training_samples, batch_size),
+                              samples_per_epoch=samples_per_epoch,
+                              nb_epoch=n_epoch, verbose=2,
+                              callbacks=[model_checkpoint, early_stopping,
+                                         reduce_LR_plateau, tensorboard_log],
+                              validation_data=gen.generator(validation_samples, batch_size),
+                              nb_val_samples = nb_val_samples,
+                              initial_epoch = begin_at_epoch)
+    print("Done Training")
 	
 	
 ##Function to save model architecture
 def save_model_JSON(model, filename):
-	print("Saving model architecture");
-	model_json = model.to_json();
-	with open (filename, 'w') as json_file:
-	    json.dump(model_json, json_file, indent=4, sort_keys=True, separators=(',', ':'))
-	print("Model saved to ", filename, " in current directory");
+    print("Saving model architecture");
+    model_json = model.to_json();
+    with open (filename, 'w') as json_file:
+        json.dump(model_json, json_file, indent=4, sort_keys=True, separators=(',', ':'))
+    print("Model saved to ", filename, " in current directory");
 
 ##Function to load model architecture
 def load_model_JSON(filename):
-	if os.path.isfile(filename):
-		with open(filename, 'r') as json_file:
-			model = model_from_json(json_file.load())
-		return model
-	return None
+    if os.path.isfile(filename):
+        with open(filename, 'r') as json_file:
+            model = model_from_json(json_file.load())
+            return model
+    return None
 
 ##Function to save entire model (architecture+weight)
-def save_model(model, filename):
-	model.save(filename)  # creates a HDF5 file
-	return
+def save_net(model, filename):
+    model.save(filename)  # creates a HDF5 file
+    return
 
 # Function to load compiled model(architecture+weight)
-def load_model(filename):
-	if os.path.isfile(filename):
-		model = load_model(filename)
-		return model
-	return None;
+def load_net(filename):
+    if os.path.isfile(filename):
+        model = load_model(filename)
+        return model
+    return None;
+
+#Function takes a vector and generates a 1D Gaussian Signal, with mu and sigma
+def gauss_1D(x, mu, sigma):
+    return np.exp(-(x - float(mu))**2 / (2*float(sigma)**2))
+
+#Function to generate 2D Gaussian Kernel given a mean, sigma
+def gauss_kernel(kernel_size, mu, sigma):
+    kernel_size = max(3, kernel_size)
+    x = np.linspace(-kernel_size, kernel_size, kernel_size)
+    x_g = gauss_1D(x, mu, sigma)
+    
+    kernel = np.outer(np.transpose(x_g), x_g)
+    kernel = kernel / np.sum(kernel)
+    return kernel
