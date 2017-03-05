d9223b5c8786de1533e38547ddf4d4fa894e0472
Ankur Agarwal <ankuraga@gmail.com>
2017-03-05 03:30:53 -0800

    Adding brightness splatter noise

--- data_generator.py
+++ data_generator.py
@@ -18,23 +18,50 @@ def augment_data(images, angles, affine_jitter=0):
     flip_images, flip_angles = flip_data(images, angles)
     images = np.vstack((images, flip_images));
     angles = np.hstack((angles, flip_angles));
-    
+                      
+    bright_images, bright_angles = add_brightness_noise(images, angles)
+    images = np.vstack((images, bright_images));
+    angles = np.hstack((angles, bright_angles));
     return images, angles
 
 def flip_data(images, angles):
     flip_images = images[:, :, ::-1, :];
     flip_angles = -angles;
     return flip_images, flip_angles
-    
+
+def add_brightness_noise(images, angles):
+    new_images = []
+    new_angles = []
+    for img, angles in zip(images, angles):
+        hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        #Add randowm noise to L channel
+        #We are making the images light or dark
+        hls[:,:,1] = hls[:,:,1] + np.random.uniform(-30, 30, img.shape[0:-1])
+        new_images.append(cv2.cvtColor(hls, cv2.COLOR_HLS2RGB))
+        new_angles.append(angles)
+    return new_images, new_angles
+
+def crop_image(image):
+    image = image[65:-25,:,:]
+    return image
+
+def resize_image(image):
+    image = cv2.resize(image,(64, 64), interpolation = cv2.INTER_AREA)
+    return image
+
+def preproc_image(image):
+    return resize_image(crop_image(image))
     
 def jitter(images, angles):
     return
+
+
 
 ##Function generates batch_size of data from the samples provided
 #Input: samples: list of samples from the data .csv
 #       batch_size: size of generated data (4th dim)
 
-def generator(samples, batch_size=32):
+def generator(samples, batch_size=32, drop_prob=0.4):
     correction = 0.2;
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
@@ -43,22 +70,36 @@ def generator(samples, batch_size=32):
             batch_samples = samples[offset:offset+batch_size]
             images = []
             angles = []
+            
             for batch_sample in batch_samples:
-                image_name = batch_sample[0]
-                image = cv2.imread(image_name)
-                angle = float(batch_sample[3])
-                images.append(image)
-                angles.append(angle)
+                image_name = batch_sample[0].strip()
                 
+                image = cv2.imread(image_name)
+                
+                angle = float(batch_sample[3])
+                if angle == 0.0:
+                    if np.random.random() < drop_prob:
+                        continue
+                    
+                image = preproc_image(image)
+                images.append(image)
+                
+                angles.append(angle)
                 #[(left_img_idx, +ve correction), (right_img_idx, -ve correction)]
                 for idx, sign in [(1, 1), (2, -1)]:
-                    image_name = batch_sample[idx]
+                    image_name = batch_sample[idx].strip()
                     image = cv2.imread(image_name)
+                    
+                    image = preproc_image(image)
                     images.append(image)
                     angles.append(angle + (sign*correction))
-                
+            
+            if len(images) == 0:
+                continue
+            
             # trim image to only see section with road
             X_train = np.array(images)
+            
             y_train = np.array(angles)
             X_train, y_train = augment_data(X_train, y_train)
             
--- generator_test.py
+++ generator_test.py
@@ -36,26 +36,28 @@ def plot_image_data(train_batch_X, train_batch_y):
     plt.show()
     
     
-data_dir = '../BehavClone_training';
-if not os.path.isdir(data_dir):
-    print("data directory doesn't exist")
-
-csv_file = os.path.join(data_dir, 'driving_log.csv')
-if not os.path.isfile(csv_file):
-    print("Could not find CSV file")
-
-image_dir = os.path.join(data_dir, 'IMG')
-if not os.path.isdir(image_dir):
-    print("Could not find image directory")
-    
 samples = []
-with open(csv_file) as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        samples.append(line)
+data_dir = ['./'];
+for training_dir in data_dir:
+    if not os.path.isdir(training_dir):
+        print("data directory doesn't exist")
+
+    csv_file = os.path.join(training_dir, 'driving_log.csv')
+    if not os.path.isfile(csv_file):
+        print("Could not find CSV file")
+
+    image_dir = os.path.join(training_dir, 'IMG')
+    if not os.path.isdir(image_dir):
+        print("Could not find image directory")
+    
+    print(csv_file)
+    with open(csv_file) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            samples.append(line)
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-train_generator = generator(train_samples, batch_size=5)
-validation_generator = generator(validation_samples, batch_size=5)
+train_generator = generator(train_samples, batch_size=5, drop_prob=0)
+validation_generator = generator(validation_samples, batch_size=1)
 train_batch_X, train_batch_y = next(train_generator)
 plot_image_data(train_batch_X, train_batch_y)
